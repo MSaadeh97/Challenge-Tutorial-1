34d2cc327772834c2d4ed5f3af2af2be4c109f77
MSaadeh97 <59637290+MSaadeh97@users.noreply.github.com>
2020-01-30 23:06:31 -0500

    Challenge Quest

---
+++ Tutorial 1/Tutorial 1/Assets/TutorialInfo/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    private Rigidbody2D rb2d;
+    private int lives;
+
+    private int count;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        SetLivesText();
+        SetCountText();
+        winText.text = "";
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        if (count == 12)
+        {
+            transform.position = new Vector2(117, 3);
+        }
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);           
+            lives = lives - 1;
+            SetLivesText();
+            SetCountText();
+        }
+
+
+
+
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        
+        if (count >= 20)
+        {
+            winText.text = "You Win! Game created by Mohammad Saadeh!";
+        }
+        if (lives == 0)
+        {
+            winText.text = "You Lose! Game created by Mohammad Saadeh!";
+            Destroy(this);
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+    }
+}
